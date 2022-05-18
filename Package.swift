24c366a25897aec3498784b3439aa1045c580053
fedor <fedor.korotkov@gmail.com>
2022-05-18 10:21:48 -0400

    Fixes after rebase

--- Package.swift
+++ Package.swift
@@ -15,10 +15,18 @@ let package = Package(
     .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.9.2"),
   ],
   targets: [
-    .executableTarget(name: "tart", dependencies: [
-      .product(name: "ArgumentParser", package: "swift-argument-parser"),
-      .product(name: "Parsing", package: "swift-parsing"),
-    ]),
-    .testTarget(name: "TartTests", dependencies: ["tart"])
+    .executableTarget(name: "tart",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        .product(name: "Parsing", package: "swift-parsing"),
+      ],
+      plugins: [.plugin(name: "SwiftEntitlementsPlugin")]),
+    .testTarget(name: "TartTests", dependencies: ["tart"]),
+    .plugin(
+      name: "SwiftEntitlementsPlugin",
+      capability: .buildTool(),
+      dependencies: ["codesign"]
+    ),
+    .binaryTarget(name: "codesign", path: "artifacts/codesign.artifactbundle")
   ]
 )
